fff089366bfae6d133401465596f919e409c1a21
hayawata3626 <top.men09@gmail.com>
2018-08-08 09:18:15 +0900

    コンポーネントを追加

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+export type theme = {
+  primary: string
+  primaryVariant: string
+  secondary: string
+  secondaryVariant: string
+  onPrimary: string
+  onSecondary: string
+}
+
+export const theme = {
+  primary: '#6200EE',
+  primaryVariant: '#3700B3',
+  secondary: '#03DAC6',
+  secondaryVariant: '#018786',
+  onPrimary: '#fff',
+  onSecondary: '#000'
+}
